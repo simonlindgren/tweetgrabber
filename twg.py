d423145fe58d7e860846b9f8f31a0a4b686a4836
simonlindgren <simon.lindgren@umu.se>
2020-03-26 22:41:36 +0000

    Minor edit

--- twg.py
+++ twg.py
@@ -58,31 +58,29 @@ def dbcreate(project):
 # Set up the listener
 class MyStreamListener(tweepy.StreamListener):
     def on_data(self, data):
-        try:
-            conn = sqlite3.connect(args.project + ".db")
-            data = json.loads(data)
-            tweet_id = int(data['id'])
-            tweet_created_at = data['created_at']
-            tweet_author = data['user']['screen_name']
-            author_location = data['user']['location']
-            author_followers = data['user']['followers_count'] if not None else 0
-            author_friends = data['user']['friends_count'] if not None else 0
-            hashtags = data['entities']['hashtags']
-            tweet_hashtags = []
-            for hashtag in hashtags:
-                tweet_hashtags.append("#" + str(hashtag['text']))
-            tweet_hashtags = ",".join(tweet_hashtags)
-            tweet_text = data['text']
-            in_reply_to = data['in_reply_to_screen_name']
-            tweet_lang = data['lang']
-            conn.execute('INSERT INTO tweets (id, created_at, author, author_location, author_followers, author_friends, hashtags, tweet, in_reply_to, lang, method) VALUES (?,?,?,?,?,?,?,?,?,?,?)', (tweet_id, tweet_created_at, tweet_author, author_location, author_followers, author_friends, tweet_hashtags, tweet_text, in_reply_to, tweet_lang, "StreamingAPI"))
-            conn.commit()
-            cursor = conn.cursor()
-            cursor.execute("select * from tweets")
-            r = cursor.fetchall()       
-            print("\r======= Tweet from " + str(tweet_created_at[:-10]) + "(" + str(len(r)) +")", end="")
-        except:
-            pass
+        conn = sqlite3.connect(args.project + ".db")
+        data = json.loads(data)
+        tweet_id = int(data['id'])
+        tweet_created_at = data['created_at']
+        tweet_author = data['user']['screen_name']
+        author_location = data['user']['location']
+        author_followers = data['user']['followers_count'] if not None else 0
+        author_friends = data['user']['friends_count'] if not None else 0
+        hashtags = data['entities']['hashtags']
+        tweet_hashtags = []
+        for hashtag in hashtags:
+            tweet_hashtags.append("#" + str(hashtag['text']))
+        tweet_hashtags = ",".join(tweet_hashtags)
+        tweet_text = data['text']
+        in_reply_to = data['in_reply_to_screen_name']
+        tweet_lang = data['lang']
+        conn.execute('INSERT INTO tweets (id, created_at, author, author_location, author_followers, author_friends, hashtags, tweet, in_reply_to, lang, method) VALUES (?,?,?,?,?,?,?,?,?,?,?)', (tweet_id, tweet_created_at, tweet_author, author_location, author_followers, author_friends, tweet_hashtags, tweet_text, in_reply_to, tweet_lang, "StreamingAPI"))
+        conn.commit()
+        cursor = conn.cursor()
+        cursor.execute("select * from tweets")
+        r = cursor.fetchall()       
+        print("\r======= Tweet from " + str(tweet_created_at[:-10]) + "(" + str(len(r)) +")", end="")
+
    
        
 def stream(project):
