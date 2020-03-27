3c28946e1eeec54ced1d5c17f88cff0d1dfad94c
simonlindgren <simon.lindgren@umu.se>
2020-03-27 07:33:31 +0000

    Add parameters.py

---
+++ parameters.py
@@ -0,0 +1,14 @@
+# Edit to set your desired project name
+projectname = "twg"
+
+# Edit the search query
+# Keywords as comma-separated strings
+# e.g. "monkey,#monkeytime,#2020"
+# Leave empty if only streaming based on user accounts
+keywords = "coffee,love"
+
+# Enter user accounts as comma-separated strings
+# e.g. "759251,37706685,19701628"
+# Leave empty if only streaming based on text query
+users = ""
+
